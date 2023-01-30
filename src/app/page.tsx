c948c461fd630587b57edcaabcbe058c92b232a3
Zak Mayfield <zakmayfield@gmail.com>
2023-01-30 11:53:38 -0500

    testing tailwind with base styles

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,9 @@
-
 export default function Home() {
   return (
-    <main className=''>
-      
+    <main className='text-center p-2'>
+      <div>
+        <h1 className='underline'>Collars Client V2</h1>
+      </div>
     </main>
   )
 }
